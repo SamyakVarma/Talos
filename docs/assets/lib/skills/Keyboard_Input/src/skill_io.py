eb490f82d6ce4cb373e4b01f0d8c0cc5d98ab45d
SamyakVarma <153301174+SamyakVarma@users.noreply.github.com>
2025-12-22 13:28:33 +0530

    Delete docs directory

--- docs/assets/lib/skills/Keyboard_Input/src/skill_io.py
+++
@@ -1,7 +0,0 @@
-class KeyboardInput_IP:
-    def __init__(self):
-        self.keys = []
-        
-class KeyboardInput_OP:
-    def __init__(self):
-        self.pressed = []
--- docs/assets/lib/skills/Skill_template/src/skill_io.py
+++
@@ -1,7 +0,0 @@
-class <skill_ID>_IP:
-    def __init__(self):
-        self.attribute1 = #default
-        
-class <skill_ID>_OP:
-    def __init__(self):
-         self.attribute1 = #default
