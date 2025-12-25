ac56e539aa11db374cb4766c0ebd23d11e1219a4
SamyakVarma <nssamyakvarma@gmail.com>
2025-12-25 22:20:06 +0530

    Rectified dunamic output paths

--- assets/lib/std_skills/Skill_template/src/main.py
+++ assets/lib/std_skills/Skill_template/src/main.py
@@ -21,7 +21,8 @@ T_O_P = os.path.abspath(os.path.join(CURRENT, "..", "..", "..", "out"))
 S_A_P = os.path.abspath(os.path.join(CURRENT, "..", "..", ".."))
 os.makedirs(T_O_P, exist_ok=True)
 CONF_FILE = os.path.join(S_A_P, "config.yaml")
-OUTPUT_FILE = os.path.join(T_O_P, "{{skill_id}}_out.glob")
+LOCAL_SKILL_ID = os.path.basename(S_A_P)
+OUTPUT_FILE = os.path.join(T_O_P, f"{LOCAL_SKILL_ID}_out")
 
 from user_main import userMain
 
